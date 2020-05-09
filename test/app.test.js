9430f045242e3a5efbe2c9db69bd3c8728f00c8e
David Queen <dsqjr@msn.com>
2020-05-09 09:26:23 -0400

    started server

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,25 @@
 const app = require('../src/app');
-describe('server response with 200 and Hello world',()=>{
-  it('resonse with a 200 code',()=>{
+describe('server response with 200 on all valid URL',()=>{
+  it('resonse with a 200 code at root',()=>{
     return request(app).get('/').expect(200);
+  });
+
+  it('response at /join',()=>{
+    return request(app).get('/join').expect(200);
+  });
+  it('response at /create',()=>{
+    return request(app).get('/create').expect(200);
+  });
+  it('response at /questions',()=>{
+    return request(app).get('/questions').expect(200);
+  });
+  
+});
+
+describe('each endpoint returns appropriate response',()=>{
+  describe('/create',()=>{
+    it('returns a random key',()=>{
+      return request(app).get('/create').expect();
+    });
   });
 });
